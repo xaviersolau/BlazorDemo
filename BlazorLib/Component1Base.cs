51a6f2cba96ade12594333f73794feb6a64837e1
Xavier Solau <x.solau@criteo.com>
2019-06-03 11:50:08 +0200

    Add Component1Base class to separate View and Logic.

---
+++ BlazorLib/Component1Base.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorLib
+{
+    public class Component1Base : ComponentBase
+    {
+        [Inject]
+        private IJSRuntime JsRuntime { get; set; }
+
+        [Parameter]
+        protected string Message { get; set; } = string.Empty;
+
+        protected string JsMessage { get; set; } = string.Empty;
+
+        protected async Task CallExampleJsInterop()
+        {
+            JsMessage = await ExampleJsInterop.Prompt(JsRuntime, "Message from .Net!");
+        }
+    }
+}
