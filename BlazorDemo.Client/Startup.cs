5d522b958f79fc4841175ec1e5add00c7cf10146
Xavier Solau <xaviersolau@yahoo.fr>
2019-10-09 00:20:45 +0200

    Add ActionDispatcher on BlazorDemo client side.

--- BlazorDemo.Client/Startup.cs
+++ BlazorDemo.Client/Startup.cs
@@ -1,6 +1,10 @@
+using BlazorDemo.Client.State;
+using BlazorLib.State;
 using Microsoft.AspNetCore.Components.Builder;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using SoloX.ActionDispatch.Core;
+using SoloX.ActionDispatch.Core.State;
 
 namespace BlazorDemo.Client
 {
@@ -8,6 +12,22 @@ namespace BlazorDemo.Client
     {
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<IStateFactoryProvider, Impl.StateFactoryProvider>();
+            services.AddSingleton<IStateFactoryProvider, BlazorLib.Impl.StateFactoryProvider>();
+
+            services.AddActionDispatchSupport(
+                factory =>
+                {
+                    var state = factory.Create<IAppState>();
+                    state.Counter = factory.Create<ICounterState>();
+                    state.Counter.MyCounter = factory.Create<IMyCounterState>();
+                    state.MyCounter = factory.Create<IMyCounterState>();
+
+                    state.Counter.MyCounter.Count = 5;
+                    state.MyCounter.Count = 15;
+
+                    return state;
+                });
         }
 
         public void Configure(IComponentsApplicationBuilder app)
